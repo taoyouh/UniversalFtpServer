9d789814bfc3b42aae6689754996d544e39cbbe1
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-11-07 10:40:17 +0800

    Localization prepare

--- UniversalFtpServer/MainPage.xaml.cs
+++ UniversalFtpServer/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
@@ -29,6 +30,15 @@ namespace UniversalFtpServer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        const string Ipv4Listening = "MainPage_Ipv4Listening";
+        const string Ipv6Listening = "MainPage_Ipv6Listening";
+        const string Ipv4Error = "MainPage_Ipv4Error";
+        const string Ipv6Error = "MainPage_Ipv6Error";
+        const string Ipv4Stopped = "MainPage_Ipv4Stopped";
+        const string Ipv6Stopped = "MainPage_Ipv6Stopped";
+        const string Ok = "Dialog_Ok";
+        const string PortIncorrect = "MainPage_PortIncorrect";
+
         FtpServer server4;
         Task server4Run;
         FtpServer server6;
@@ -47,9 +57,11 @@ namespace UniversalFtpServer
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            var loader = new ResourceLoader();
+
             if (!int.TryParse(portBox.Text, out int port))
             {
-                NotifyUser("PortIncorrect");
+                NotifyUser(loader.GetString(PortIncorrect));
                 return;
             }
 
@@ -63,8 +75,8 @@ namespace UniversalFtpServer
 
             var x = VisualStateManager.GoToState(this, nameof(runningState), true);
 
-            statusBlock4.Text = $"Server at IPv4 is listening at port {port}";
-            statusBlock6.Text = $"Server at IPv6 is listening at port {port}";
+            statusBlock4.Text = string.Format(loader.GetString(Ipv4Listening), port);
+            statusBlock6.Text = string.Format(loader.GetString(Ipv6Listening), port);
 
             cts = new CancellationTokenSource();
             IPEndPoint ep4 = new IPEndPoint(IPAddress.Any, port);
@@ -96,27 +108,28 @@ namespace UniversalFtpServer
                 await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
                     if (t.IsFaulted)
-                        statusBlock4.Text = $"Server at IPv4 error: \n{t.Exception}";
+                        statusBlock4.Text = string.Format(loader.GetString(Ipv4Error), t.Exception);
                     else
-                        statusBlock4.Text = "Server at IPv4 has stopped.";
+                        statusBlock4.Text = loader.GetString(Ipv4Stopped);
 
                 }));
             server6Run = server6.RunAsync(cts.Token).ContinueWith(async t =>
                 await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
                     if (t.IsFaulted)
-                        statusBlock6.Text = $"Server at IPv6 error: \n{t.Exception}";
+                        statusBlock6.Text = string.Format(loader.GetString(Ipv6Error), t.Exception);
                     else
-                        statusBlock6.Text = "Server at IPv6 has stopped.";
+                        statusBlock6.Text = loader.GetString(Ipv6Stopped);
                 }));
         }
 
         private void NotifyUser(string v)
         {
+            var loader = new ResourceLoader();
             ContentDialog dialog = new ContentDialog
             {
                 Content = v,
-                CloseButtonText = "Ok"
+                CloseButtonText = loader.GetString(Ok)
             };
             var result = dialog.ShowAsync();
         }
