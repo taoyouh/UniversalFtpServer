4a1a91948182d7aba2f84b90a0966c250db08ce4
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-12-06 08:28:42 +0800

    Add publish scripts to version control

---
+++ Publish/InstallStoreBroker.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "Cleaning Store Broker folder" -ForegroundColor Cyan
+$installFolder = [System.IO.Path]::Combine($Home, "Documents\WindowsPowerShell\Modules\StoreBroker");
+if (Test-Path -Path $installFolder)
+{
+    Remove-Item -Force -Recurse -Path $installFolder
+}
+New-Item -Type Directory -Force -Path $installFolder
+
+Write-Host "Installing Store Broker" -ForegroundColor Cyan
+Push-Location -Path $installFolder
+nuget install Microsoft.Windows.StoreBroker
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error ("msbuild exited with code " + $LASTEXITCODE)
+    Pop-Location
+    EXIT $LASTEXITCODE
+}
+Move-Item -Path ".\Microsoft.Windows.StoreBroker.*" -Destination ".\StoreBroker"
+Pop-Location
---
+++ Publish/PublishToStore.ps1
@@ -0,0 +1,41 @@
+﻿# 从AppxPackages中获取appxupload格式安装包
+# 从Publish\StoreBroker\Pdb中获取商店描述信息
+# 自动替换已有的包和商店描述信息，并自动提交
+
+$rootPath = Get-Location
+$tenantId = $env:PUBLISH_TENANTID
+$clientId = $env:PUBLISH_CLINETID
+$clientSecret = $env:PUBLISH_CLIENTSECRET | ConvertTo-SecureString -AsPlainText -Force
+$appId = $env:PUBLISH_APPID
+
+$outName = "Submission"
+
+$configPath = [System.IO.Path]::Combine($env:rootPath, "Publish\StoreBroker\SBConfig.json")
+$pdpRootPath = [System.IO.Path]::Combine($env:rootPath, "Publish\StoreBroker\Pdp\")
+$imageRootPath = [System.IO.Path]::Combine($env:rootPath, "Publish\StoreBroker\Images\")
+$appxPath = [System.IO.Path]::Combine($env:rootPath, "AppxPackages\")
+
+$outPath = [System.IO.Path]::Combine($env:rootPath, "Publish\SubmissionPackage\")
+
+Write-Host "Initializing submission package path" -ForegroundColor Cyan
+if (Test-Path -Path $outPath)
+{
+    Remove-Item -Force -Recurse -Path $outPath
+}
+New-Item -Type Directory -Force -Path $outPath
+
+Write-Host "Logging in to Dev Center" -ForegroundColor Cyan
+$cred = New-Object System.Management.Automation.PSCredential $clientId, $clientSecret
+Set-StoreBrokerAuthentication -TenantId $tenantId -Credential $cred
+
+Write-Host ("Looking for appxupload at " + $appxPath) -ForegroundColor Cyan
+$appxuploads = (Get-ChildItem -Path $appxPath | Where-Object Name -like "*.appxupload")
+
+Write-Host "Creating submission package:" -ForegroundColor Cyan
+New-SubmissionPackage -ConfigPath $configPath -PDPRootPath $pdpRootPath -ImagesRootPath $imageRootPath -OutPath $outPath -OutName $outName -AppxPath $appxuploads.FullName
+
+Write-Host "Submitting package to Dev Center" -ForegroundColor Cyan
+Update-ApplicationSubmission -AppId $appId -SubmissionDataPath ($outPath + $outName + ".json") -PackagePath ($outPath + $outName + ".zip") -Force -ReplacePackages -UpdateListings -TargetPublishMode Manual -AutoCommit
+
+Write-Host "Clearing Authentication" -ForegroundColor Cyan
+Clear-StoreBrokerAuthentication
